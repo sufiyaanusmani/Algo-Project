f94955cd4d0b021445b0880e1838ad1c91cebbb9
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2023-11-14 22:53:50 +0500

    add graham scan animation code

--- algoproject/base/views.py
+++ algoproject/base/views.py
@@ -4,6 +4,7 @@ from scipy.spatial import ConvexHull as cv
 import numpy as np
 import imageio
 import os
+from functools import cmp_to_key
 
 # Create your views here.
 
@@ -14,10 +15,14 @@ class Point:
         self.y = y
 
 
-def createAnimation(output_file='animation.gif', fps=2):
+p0 = Point(0, 0)
+
+
+def createAnimation(output_file, algorithm, fps=2):
     images = []
     png_folder = 'static'
-    png_files = [f for f in os.listdir(png_folder) if f.endswith('.png')]
+    png_files = [f for f in os.listdir(
+        png_folder) if f.endswith(f'{algorithm}.png')]
     print(png_files)
     png_files.sort()
 
@@ -26,7 +31,43 @@ def createAnimation(output_file='animation.gif', fps=2):
         images.append(imageio.imread(file_path))
 
     # Save the animation
-    imageio.mimsave(output_file, images, fps=fps)
+    imageio.mimsave(output_file, images, fps=fps, loop=10)
+
+
+def orientation(p, q, r):
+    val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
+
+    if val == 0:
+        return 0
+    elif val > 0:
+        return 1
+    else:
+        return 2
+
+
+def distSq(p1, p2):
+    return ((p1.x - p2.x) * (p1.x - p2.x) +
+            (p1.y - p2.y) * (p1.y - p2.y))
+
+
+def compare(p1, p2):
+
+    # Find orientation
+    o = orientation(p0, p1, p2)
+    if o == 0:
+        if distSq(p0, p2) >= distSq(p0, p1):
+            return -1
+        else:
+            return 1
+    else:
+        if o == 2:
+            return -1
+        else:
+            return 1
+
+
+def nextToTop(S):
+    return S[-2]
 
 
 class ConvexHull:
@@ -44,16 +85,6 @@ class ConvexHull:
                     minn = i
         return minn
 
-    def orientation(self, p, q, r):
-        val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
-
-        if val == 0:
-            return 0
-        elif val > 0:
-            return 1
-        else:
-            return 2
-
     def jarvisMarch(self):
         n = len(self.points)
         if n < 3:
@@ -70,11 +101,8 @@ class ConvexHull:
             q = (p + 1) % n
 
             for i in range(n):
-
-                # If i is more counterclockwise
-                # than current q, then update q
-                if (self.orientation(self.points[p],
-                                     self.points[i], self.points[q]) == 2):
+                if (orientation(self.points[p],
+                                self.points[i], self.points[q]) == 2):
                     q = i
             p = q
 
@@ -84,10 +112,64 @@ class ConvexHull:
         i = 1
         for each in hull:
             self.hull.append(Point(self.points[each].x, self.points[each].y))
-            self.saveGraph(f'static/jarvis{i}.png')
+            self.saveGraph(f'static/{i}jarvis.png')
             i += 1
 
-        createAnimation(output_file='static/jarvis.gif')
+        createAnimation(output_file='static/convexhull.gif',
+                        algorithm='jarvis')
+
+    def grahamScan(self):
+        self.hull = []
+        points = self.points[:]
+        n = len(points)
+        ymin = points[0].y
+        min = 0
+        for i in range(1, n):
+            y = points[i].y
+
+            if ((y < ymin) or
+                    (ymin == y and points[i].x < points[min].x)):
+                ymin = points[i].y
+                min = i
+
+        points[0], points[min] = points[min], points[0]
+
+        p0 = points[0]
+        points = sorted(points, key=cmp_to_key(compare))
+
+        m = 1
+        for i in range(1, n):
+
+            while ((i < n - 1) and
+                   (orientation(p0, points[i], points[i + 1]) == 0)):
+                i += 1
+
+            points[m] = points[i]
+            m += 1
+
+        if m < 3:
+            return
+
+        self.hull.append(points[0])
+        self.saveGraph(f'static/1graham.png')
+        self.hull.append(points[1])
+        self.saveGraph(f'static/2graham.png')
+        self.hull.append(points[2])
+        self.saveGraph(f'static/3graham.png')
+        i = 4
+
+        for i in range(3, m):
+
+            while ((len(self.hull) > 1) and (orientation(nextToTop(self.hull), self.hull[-1], points[i]) != 2)):
+                self.hull.pop()
+                i -= 1
+                self.saveGraph(f'static/{i}graham.png')
+            self.hull.append(points[i])
+            self.saveGraph(f'static/{i}graham.png')
+            i += 1
+
+        createAnimation(output_file='static/convexhull.gif',
+                        algorithm='graham')
 
     def saveGraph(self, fileName):
         allX = []
@@ -147,26 +229,30 @@ def linesIntersectionMethod2Page(request):
 def convexHullPage(request):
     totalPoints = 0
     t = []
+    algorithm = ''
     pointsEntered = False
     if request.method == 'POST':
         if 'totalPoints' in request.POST:
-            print(request.POST['totalPoints'])
             totalPoints = int(request.POST['totalPoints'])
             for i in range(1, totalPoints+1):
                 t.append(i)
             pointsEntered = True
         else:
-            p = list(dict(request.POST).values())[1:]
+            p = list(dict(request.POST).values())[1:-1]
             i = 0
+            algorithm = request.POST['algorithm']
             convexHull.points = []
             while i < len(p):
                 # points.append([int(p[i][0]), int(p[i+1][0])])
                 convexHull.points.append(Point(int(p[i][0]), int(p[i+1][0])))
                 i += 2
-            convexHull.jarvisMarch()
+            if algorithm == 'jarvismarch':
+                convexHull.jarvisMarch()
+            elif algorithm == 'grahamscan':
+                convexHull.grahamScan()
             return redirect('convex-hull-result-page')
     context = {'pageName': "Convex Hull Page",
-               'pointsEntered': pointsEntered, 'totalPoints': t}
+               'pointsEntered': pointsEntered, 'totalPoints': t, 'algorithm': algorithm}
     return render(request, 'base/convex-hull.html', context)
 
 
